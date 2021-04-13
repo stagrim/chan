c1735dd11aced8fd093f55f9a8a06968f93dfdb0
Esbjörn Stenberg <me@stagrim.com>
2021-04-13 16:25:58 +0200

    Get links, but not from 4chan

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,70 @@
 extern crate clap;
+extern crate reqwest;
+extern crate select;
+
+use select::{document::Document, predicate::Name};
+// use futures::executor::block_on;
 
 mod cli;
 
 fn main() {
     let matches = cli::build_cli().get_matches();
-    
-    println!("Iqdb: {}", matches.is_present("Iqdb"));
-    println!("Directory: {}", matches.is_present("Directory"));
-    if matches.is_present("Directory") {
-        println!("\tValue: {}", matches.value_of("Directory").unwrap());
+    let url: &str = matches.value_of("url").expect("No url provided");
+    let dir: &str;
+    let mut number: i32 = 0;
+    let urls: Vec<String>;
+
+    if matches.value_of("directory").is_some() {
+        dir = matches.value_of("directory").unwrap();
     }
-    println!("Override: {}", matches.is_present("Override"));
-    println!("Print-numbered: {}", matches.is_present("Print-numbered"));
-    println!("Quiet: {}", matches.is_present("Quiet"));
-    println!("Debug: {}", matches.is_present("Debug"));
-    println!("Url: {}", matches.value_of("URL").unwrap());
+    else {
+        dir = url.split("/")
+                .filter(|&s| !s.is_empty())
+                .collect::<Vec<_>>()
+                .last()
+                .expect("No directory name could be created");
+    }
+    
+    println!("Downloading images to {}/", dir);
+    println!("Url: {}", url);
+
+    if matches.is_present("iqdb") {
+        urls = get_links(url).into_iter()
+                .filter(|n| 
+                    !n.contains("url=") && 
+                    n.contains("/thumb/"))
+                .collect();
+    }
+    else {
+        urls = get_links(url).into_iter()
+                .filter(|n| 
+                    !n.contains("url=") && 
+                    !n.contains("/thumb/"))
+                .collect();
+    }
+
+    for url in urls {
+        println!("{}", url);
+    }
+    
+}
+
+fn get_links(url: &str) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
+    let resp = reqwest::blocking::get(url).unwrap();
+    println!("{:#?}", resp);
+    // assert!(resp.status().is_success(), "Connection could not be made");
+
+    Document::from_read(resp)
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .filter(|n| 
+            n.ends_with(".jpg") || 
+            n.ends_with(".gif") || 
+            n.ends_with(".png") || 
+            n.ends_with(".jpeg") || 
+            n.ends_with(".webm"))
+        .for_each(|n| res.push(n.to_string()));
+    return res
 }
